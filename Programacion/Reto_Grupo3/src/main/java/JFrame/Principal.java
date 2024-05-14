96f8ffd90612bce676074759328ad719abacb7a2
Hansdreams <gustavorodrigo.bautistapocohuanca@iesmiguelherrero.com>
2024-05-15 01:25:21 +0200

    Se genero javaDoc y se puso comentario a Acceso BD y Activida Programada

--- Programacion/Reto_Grupo3/src/main/java/JFrame/Principal.java
+++ Programacion/Reto_Grupo3/src/main/java/JFrame/Principal.java
@@ -43,7 +43,6 @@ import Ficheros.MetodoFichero;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -2665,7 +2664,9 @@ public class Principal extends javax.swing.JFrame {
 
             }
 
-        } else {
+        }else if (auxUsu == null){
+                 mensaje("Usuario no existe en la base de datos!!!");
+                } else {
 
             mensaje("Usuario o Contraseña incorrecta");
 
