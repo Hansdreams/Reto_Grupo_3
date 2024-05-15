be67f53db7974af088c0e22e93cda9e215171b6c
EloyED <152644580+EloyED@users.noreply.github.com>
2024-05-15 21:24:43 +0200

    Update TransporteDAOImp.java

--- Programacion/Reto_Grupo3/src/main/java/DAOImp/TransporteDAOImp.java
+++ Programacion/Reto_Grupo3/src/main/java/DAOImp/TransporteDAOImp.java
@@ -32,17 +32,19 @@ public class TransporteDAOImp implements MetodosBD<Transporte>{
 
     @Override
     public List<Transporte> listar() {
-        
+        // Lista para almacenar los transportes
         List<Transporte> transporte = new ArrayList<>();
-        
+        // Consulta SQL para obtener todos los transportes
         final String sql = "SELECT idTransportes,TipoTransporte FROM transportes";
         
         try ( Statement stmt = getConnection().createStatement();  ResultSet rs = stmt.executeQuery(sql);) {
-            
+            // Recorrer el resultado de la consulta
             while (rs.next()) {
-                
+                // Crear objeto Transporte a partir de los datos de la consulta
                 Transporte solicitud = crearTransporte(rs);
+                // Agregar el transporte a la lista
                 if (!transporte.add(solicitud)) {
+                    // Lanzar una excepción si no se pudo agregar el transporte a la lista
                     throw new Exception("error no se ha insertado el objeto en la colección");
                 }
                 
@@ -58,22 +60,26 @@ public class TransporteDAOImp implements MetodosBD<Transporte>{
         return transporte;
         
     }
-
+    //Método no utilizado
     @Override
     public Transporte buscar(int ID) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
+    //Método no utilizado
     @Override
     public void guardar(Transporte t) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
+    //Método no utilizado
     @Override
     public void modificar(Transporte t) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+    /**
+     * Crea un objeto Transporte a partir de un conjunto de resultados de una consulta SQL.
+     * @param rs El ResultSet que contiene los resultados de la consulta SQL.
+     * @return Un objeto Transporte creado a partir de los resultados de la consulta.
+     */
     private Transporte crearTransporte(final ResultSet rs) throws SQLException {
         return new Transporte(rs.getInt("idTransportes"), rs.getString("TipoTransporte"));
     }
